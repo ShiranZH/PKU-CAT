f57b1503cc280dc1709da8f03961e95ce68a6204
AngelaEcho <zsr060540@163.com>
2020-04-16 20:57:52 +0800

    feat: init UI interface

---
+++ src/frontend/app/src/main/java/com/example/pkucat/ui/setting/SettingFragment.java
@@ -0,0 +1,34 @@
+package com.example.pkucat.ui.setting;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.pkucat.R;
+
+public class SettingFragment extends Fragment {
+    private SettingViewModel settingViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        settingViewModel =
+                ViewModelProviders.of(this).get(SettingViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_setting, container, false);
+        final TextView textView = root.findViewById(R.id.text_setting);
+        settingViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
