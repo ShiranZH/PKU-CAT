08a863eeec8907e95cb657a62d400b2ae923427c
AngelaEcho <zsr060540@163.com>
2020-04-27 15:01:42 +0800

    fix: recover removed files

--- src/frontend/app/src/main/java/com/example/pkucat/ui/post/PostActivity.java
+++ src/frontend/app/src/main/java/com/example/pkucat/ui/post/PostActivity.java
@@ -1,13 +1,26 @@
 package com.example.pkucat.ui.post;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.pkucat.R;
+import com.example.pkucat.ui.post.list.SectionsPagerAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -15,6 +28,44 @@ public class PostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        // 设置名称
+        setTitle("Cat Community");
+
+        // 返回键
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs_post);
+        tabs.setupWithViewPager(viewPager);
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String path="https://49.235.56.155/posts/?limit=10&start=10";
+                try {
+                    try{
+                        URL url = new URL(path); //新建url并实例化
+                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                        connection.setRequestMethod("GET");//获取服务器数据
+                        connection.setReadTimeout(8000);//设置读取超时的毫秒数
+                        connection.setConnectTimeout(8000);//设置连接超时的毫秒数
+                        InputStream in = connection.getInputStream();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                        String result = reader.readLine();//读取服务器进行逻辑处理后页面显示的数据
+                        System.out.println(result);
+                    }catch (MalformedURLException e){}
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
 
         FloatingActionButton fab = findViewById(R.id.fab_post);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -25,5 +76,17 @@ public class PostActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // 返回键
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish(); // back button
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
 
---
+++ src/frontend/app/src/main/java/com/example/pkucat/ui/post/PostEntity.java
@@ -0,0 +1,62 @@
+package com.example.pkucat.ui.post;
+
+import android.net.Uri;
+
+import java.io.Serializable;
+
+public class PostEntity implements Serializable {
+    private String avatarPath;
+    private String userName;
+    private String postTime;
+    private String postContent;
+    private String imagePath;
+
+    public PostEntity(String avatarPath, String userName, String postTime, String postContent, String imagePath) {
+        this.avatarPath = avatarPath;
+        this.userName = userName;
+        this.postTime = postTime;
+        this.postContent = postContent;
+        this.imagePath = imagePath;
+    }
+
+    public PostEntity() {
+    }
+
+    public Uri getAvatarPath() {
+        Uri avatarPathUri = Uri.parse(avatarPath);
+        return avatarPathUri;
+    }
+    public void setAvatarPath(String avatarPath) {
+        this.avatarPath = avatarPath;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPostTime() {
+        return postTime;
+    }
+    public void setPostTime(String postTime) {
+        this.postTime = postTime;
+    }
+
+    public String getPostContent() {
+        return postContent;
+    }
+    public void setPostContent(String postContent) {
+        this.postContent = postContent;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+
+}
