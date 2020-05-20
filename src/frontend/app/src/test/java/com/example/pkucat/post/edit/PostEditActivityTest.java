daff6a65b1cc432b8c4fbd25fc00e5bac134b72e
AngelaEcho <zsr060540@163.com>
2020-05-20 17:08:40 +0800

    test: init

---
+++ src/frontend/app/src/test/java/com/example/pkucat/post/edit/PostEditActivityTest.java
@@ -0,0 +1,28 @@
+package com.example.pkucat.post.edit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PostEditActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onOptionsItemSelected() {
+    }
+
+    @Test
+    public void onCreateOptionsMenu() {
+    }
+
+    @Test
+    public void onActivityResult() {
+    }
+
+    @Test
+    public void onResume() {
+    }
+}
---
+++ src/frontend/app/src/test/java/com/example/pkucat/post/list/PostFragmentTest.java
@@ -0,0 +1,20 @@
+package com.example.pkucat.post.list;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PostFragmentTest {
+
+    @Test
+    public void newInstance() {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onCreateView() {
+    }
+}
