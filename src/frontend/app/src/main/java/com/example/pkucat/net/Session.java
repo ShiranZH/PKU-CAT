ddebcee15b9f9ae34e5d84f9b906b9057334a870
张颢丹 <pkuzhd@pku.edu.cn>
2020-05-29 01:19:30 +0800

    feat: add class Session
    实现一个类似于python中的requests.session

---
+++ src/frontend/app/src/main/java/com/example/pkucat/net/Session.java
@@ -0,0 +1,199 @@
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.util.UUID;
+
+public class Session {
+    private String cookie = "";
+
+    public static String json2Url(JSONObject json) throws JSONException, UnsupportedEncodingException {
+        if (null == json) {
+            return "";
+        }
+        StringBuffer url  = new StringBuffer();
+        boolean      isfist = true;
+        for (Iterator<String> it = json.keys(); it.hasNext(); ) {
+            String key = it.next();
+            if (isfist) {
+                isfist = false;
+            } else {
+                url.append("&");
+            }
+            url.append(URLEncoder.encode(key, "utf-8")).append("=");
+            String value = json.getString(key);
+            url.append(URLEncoder.encode(value, "utf-8"));
+        }
+        return url.toString();
+    }
+    
+    public static String map2Url(Map<String, String> paramToMap) {
+        if (null == paramToMap || paramToMap.isEmpty()) {
+            return null;
+        }
+        StringBuffer url  = new StringBuffer();
+        boolean      isfist = true;
+        for (Map.Entry<String, String> entry : paramToMap.entrySet()) {
+            if (isfist) {
+                isfist = false;
+            } else {
+                url.append("&");
+            }
+            url.append(entry.getKey()).append("=");
+            String value = entry.getValue();
+            url.append(value);
+        }
+        return url.toString();
+    }
+
+    private byte[] request(String method, String urlStr, JSONObject data, HashMap<String, List<File>> files){
+        byte[] response = null;
+        try {
+            URL url = new URL(urlStr);
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            String boundary = UUID.randomUUID().toString().replace("-", "");
+            connection.setDoOutput(true);
+            connection.setRequestMethod(method);
+            connection.setRequestProperty("cookie", cookie);
+            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary="+boundary);
+            connection.setSSLSocketFactory(SSLSocketClient.getSSLSocketFactory());
+            connection.setHostnameVerifier(SSLSocketClient.getHostnameVerifier());
+            connection.connect();
+            
+            DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+            
+            if (data != null) {
+                Iterator<String> it = data.keys();
+                while (it.hasNext()) {
+                    String key = it.next();
+                    String value = data.getString(key);
+                    
+                    out.write(("--"+boundary+"\r\n").getBytes());
+                    out.write(("Content-Disposition: form-data; name=\""+URLEncoder.encode(key, "utf-8")+"\"").getBytes());
+                    out.write("\r\n\r\n".getBytes());
+                    out.write(value.getBytes());
+                }
+            }
+            if (files != null) {
+                for (Map.Entry<String, List<File>> entry : files.entrySet()) {
+                    String key = entry.getKey();
+                    List<File> fileList = entry.getValue();
+                    
+                    for (File file: fileList) {
+                        out.write(("--"+boundary+"\r\n").getBytes());
+                        out.write(("Content-Disposition: form-data; "
+                                + "name=\""+URLEncoder.encode(key, "utf-8")+"\"; "
+                                + "filename=\""+file.getName()+"\";").getBytes());
+                        out.write("\r\n\r\n".getBytes());
+                        FileInputStream in = new FileInputStream(file);
+                        out.write(IOUtils.toByteArray(in));
+                        in.close();
+                    } 
+                }
+            }
+            out.write(("\r\n--"+boundary+"--\r\n").getBytes());
+            
+            out.flush();
+            
+            String response_cookie = connection.getHeaderField("Set-Cookie");
+            if (response_cookie != null)
+                cookie = response_cookie;
+            response = IOUtils.toByteArray(connection.getInputStream());
+            
+            connection.disconnect();
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+        return response;
+    }
+    public byte[] post(String urlStr, JSONObject data, HashMap<String, List<File>> files){
+        return request("POST", urlStr, data, files);
+    }
+    
+    public byte[] put(String urlStr, JSONObject data, HashMap<String, List<File>> files){
+        return request("PUT", urlStr, data, files);
+    }
+  
+    public byte[] get(String urlStr, JSONObject data){
+        byte[] response = null;
+        try {
+            URL url = new URL(urlStr+"?"+json2Url(data));
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("cookie", cookie);
+            connection.setSSLSocketFactory(SSLSocketClient.getSSLSocketFactory());
+            connection.setHostnameVerifier(SSLSocketClient.getHostnameVerifier());
+            
+            response = IOUtils.toByteArray(connection.getInputStream());
+            
+            connection.disconnect();
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+        return response;
+    }
+}
+
+class SSLSocketClient {
+    //获取这个SSLSocketFactory
+    public static SSLSocketFactory getSSLSocketFactory() {
+        try {
+            SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, getTrustManager(), new SecureRandom());
+            return sslContext.getSocketFactory();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //获取TrustManager
+    private static TrustManager[] getTrustManager() {
+        TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType) {}
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType) {}
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    // return null; 或者
+                    return new X509Certificate[]{}; // 空实现
+                }
+            }
+        };
+        return trustAllCerts;
+    }
+
+    //获取HostnameVerifier
+    public static HostnameVerifier getHostnameVerifier() {
+        HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+            @Override
+            public boolean verify(String s, SSLSession sslSession) {
+                // true表示信任所有域名
+                return true;
+            }
+        };
+        return hostnameVerifier;
+    }
+}
