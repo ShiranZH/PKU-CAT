8c3ca254a67c3053496aed36b9c8bc403507351f
张颢丹 <pkuzhd@pku.edu.cn>
2020-06-03 22:13:11 +0800

    feat: 增加了上传动态

--- src/frontend/app/src/main/java/com/example/pkucat/net/PostManager.java
+++ src/frontend/app/src/main/java/com/example/pkucat/net/PostManager.java
@@ -1,9 +1,13 @@
 package com.example.pkucat.net;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,9 +25,9 @@ public class PostManager {
     public Post[] getPosts(int num, int start, int commentNum) throws APIException {
         try {
             JSONObject data = new JSONObject();
-//            data.put("num", String.valueOf(num));
-//            data.put("start", String.valueOf(start));
-//            data.put("commentNum", String.valueOf(commentNum));
+            data.put("limit", String.valueOf(num));
+            data.put("start", String.valueOf(start));
+            data.put("commentNum", String.valueOf(commentNum));
             byte[] ret = Session.get("/user/post/posts", data);
 
             JSONObject retData = new JSONObject(new String(ret));
@@ -44,9 +48,48 @@ public class PostManager {
         }
     }
     
-    public Post[] getPosts() throws APIException {
-        return getPosts(10, 1, 10);
+    public void addPost(String text) throws APIException {
+        JSONObject data = new JSONObject();
+        data.put("text", text);
+        data.put("isVideo", "0");
+        byte[] ret = Session.post("/user/post/post", data, null);
+        
+        JSONObject retData = new JSONObject(new String(ret));
+        if (retData.getInt("code") != 200)
+            throw new APIException(retData);
     }
+    
+    public void addPost(String text, File video, boolean isVideo) throws APIException {
+        File[] files = new File[]{video};
+        String[] urls = Session.uploadPicture(files);
+        JSONObject data = new JSONObject();
+//        JSONArray ps = new JSONArray();
+//        for (int i = 0; i < urls.length; ++i) {
+//            ps.put(urls[i]);
+//        }
+        data.put("multimediaContent", urls[0]);
+        if (isVideo)
+            data.put("isVideo", "1");
+        else
+            data.put("isVideo", "0");
+            
+        if (text != null)
+            data.put("text", text);
+        
+        try {
+            byte[] ret = Session.post("/user/post/post", data, null);
+            if (ret == null)
+                throw new APIException("404", "ÍøÂç´íÎó");
+            JSONObject retData = new JSONObject(new String(ret));
+            if (retData.getInt("code") != 200)
+                throw new APIException(retData);
+        } catch (JSONException e) {
+            throw new APIException("404", "·µ»ØÖµ´íÎó");
+        } catch (APIException e) {
+            throw e;
+        }
+    }
+    
     
     public Post[] searchPosts(String userID, String catID, String keyword, int commentNum) {
         return null;
