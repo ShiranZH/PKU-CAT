e456f212d45149a35827ba96c729bd8e00fe7a7e
bsdropD <bsdropd@gmail.com>
2020-06-03 21:19:44 +0800

    重构了权限管理部分的代码，并与后端进行对接

--- src/frontend/app/src/main/java/com/example/pkucat/manage/AdminUserList.java
+++ src/frontend/app/src/main/java/com/example/pkucat/manage/AdminUserList.java
@@ -13,7 +13,11 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import com.example.pkucat.App;
 import com.example.pkucat.R;
+import com.example.pkucat.net.APIException;
+import com.example.pkucat.net.Apply;
+import com.example.pkucat.net.Client;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -24,51 +28,73 @@ import java.util.jar.Attributes;
 public class AdminUserList extends BaseAdapter {
 
     private Context mContext;
+    private Client client;
 
-    private class UserInfo
+    private class ApplyInfo
     {
+        String applyID;
+        String userID;
+        String catID;
         String userName;
-        Integer userLevel;
-        public UserInfo(){}
-        public UserInfo(String _userName, int _userLevel)
+        String catName;
+        public ApplyInfo(){}
+        public ApplyInfo(String applyID, String userID, String catID)
         {
-            userName = _userName;
-            userLevel = _userLevel;
+            this.applyID = applyID;
+            this.userID = userID;
+            this.catID = catID;
+            try {
+                userName = client.user.getProfile(userID).username;
+                catName = client.archive.getArchive(catID).name;
+            }
+            catch (APIException e)
+            {
+                Toast.makeText(mContext, e.getDescription(), Toast.LENGTH_SHORT).show();
+            }
         }
 
         @Override
         public boolean equals(@Nullable Object obj) {
             if(obj == null) return false;
-            if(obj instanceof UserInfo)
+            if(obj instanceof ApplyInfo)
             {
-                UserInfo o = (UserInfo) obj;
-                return o.userName.equals(userName);
+                ApplyInfo o = (ApplyInfo) obj;
+                return o.applyID.equals(applyID);
             }
             return false;
         }
     }
-    ArrayList<UserInfo> userInfoList;
+    ArrayList<ApplyInfo> applyInfoList;
 
-    public AdminUserList(Context mContext)
+    public AdminUserList(Context mContext, Client client)
     {
         super();
-        this.mContext=mContext;
-        userInfoList = new ArrayList<>();
+        this.mContext = mContext;
+        this.client = client;
+        applyInfoList = new ArrayList<>();
         // example
-        userInfoList.add(new UserInfo("Alice",2));
-        userInfoList.add(new UserInfo("Bob",1));
-        userInfoList.add(new UserInfo("Carol",1));
-        userInfoList.add(new UserInfo("David",2));
+        Apply[] applyList;
+        try {
+            applyList = client.feeder.getApplys();
+            for(Apply apply: applyList)
+            {
+                applyInfoList.add(new ApplyInfo(apply.applyID, apply.userID, apply.catID));
+            }
+        }
+        catch (APIException e)
+        {
+            Toast.makeText(mContext, e.getDescription(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public int getCount() {
-        return userInfoList.size();
+        return applyInfoList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return userInfoList.get(position);
+        return applyInfoList.get(position);
     }
 
     @Override
@@ -76,54 +102,30 @@ public class AdminUserList extends BaseAdapter {
         return 0;
     }
 
-    public boolean contains(String name)
+    private void dealApply(final int index, final boolean result)
     {
-        return userInfoList.contains(new UserInfo(name,1));
-    }
-
-    public void putItem(String name, int level)
-    {
-        userInfoList.add(new UserInfo(name, level));
-    }
-
-    private class NameComparator implements Comparator<UserInfo>
-    {
-        public int compare(UserInfo x, UserInfo y)
-        {
-            return x.userName.compareToIgnoreCase(y.userName);
-        }
-    }
-    private class LevelComparator implements Comparator<UserInfo>
-    {
-        public int compare(UserInfo x, UserInfo y)
-        {
-            return x.userLevel.compareTo(y.userLevel);
-        }
-    }
-
-    public void sort_by_name()
-    {
-        Collections.sort(userInfoList, new NameComparator());
-    }
-
-    public void sort_by_level()
-    {
-        Collections.sort(userInfoList, new LevelComparator());
-    }
-
-    private void deleteUser(final int index)
-    {
-        final String name = userInfoList.get(index).userName;
+        final ApplyInfo applyInfo = applyInfoList.get(index);
+        final String name = applyInfo.userName;
+        final String applyID = applyInfo.applyID;
+        final String operation = result?"同意":"拒绝";
         AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
         builder.setTitle("提示");
-        builder.setMessage("确定要删除用户"+name+"的权限吗？");
+        builder.setMessage(operation+"用户"+name+"的申请请求吗？");
         builder.setCancelable(false);
         builder.setPositiveButton("确定", new DialogInterface.OnClickListener(){
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                userInfoList.remove(index);
-                notifyDataSetChanged();
-                Toast.makeText(mContext,"删除成功！",Toast.LENGTH_SHORT).show();
+                try {
+                    if(result)client.feeder.agreeApply(applyID);
+                    else client.feeder.refuseApply(applyID);
+                    applyInfoList.remove(index);
+                    notifyDataSetChanged();
+                    Toast.makeText(mContext,"操作成功！",Toast.LENGTH_SHORT).show();
+                }
+                catch (APIException e)
+                {
+                    Toast.makeText(mContext, "操作失败！ "+e.getDescription(),Toast.LENGTH_SHORT).show();
+                }
             }
         });
         builder.setNegativeButton("取消", new DialogInterface.OnClickListener(){
@@ -137,17 +139,25 @@ public class AdminUserList extends BaseAdapter {
     public View getView(final int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = LayoutInflater.from(mContext);
         View view = inflater.inflate(R.layout.admin_user_item,null);
-        final String name = userInfoList.get(position).userName;
-        final int level = userInfoList.get(position).userLevel;
+        final String userName = applyInfoList.get(position).userName;
+        final String catName = applyInfoList.get(position).catName;
         TextView textViewName = view.findViewById(R.id.admin_name);
-        textViewName.setText(name);
-        TextView textViewLevel = view.findViewById(R.id.admin_level);
-        textViewLevel.setText(level==1?"饲养员":"管理员");
-        Button deleteButton = view.findViewById(R.id.admin_delete_button);
-        deleteButton.setOnClickListener(new View.OnClickListener() {
+        textViewName.setText(userName);
+        TextView textViewLevel = view.findViewById(R.id.admin_cat_name);
+        textViewLevel.setText(catName);
+
+        Button agreeButton = view.findViewById(R.id.admin_agree_button);
+        agreeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                deleteUser(position);
+                dealApply(position, true);
+            }
+        });
+        Button refuseButton = view.findViewById(R.id.admin_refuse_button);
+        refuseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dealApply(position, false);
             }
         });
         return view;
--- src/frontend/app/src/main/java/com/example/pkucat/manage/ManageActivity.java
+++ src/frontend/app/src/main/java/com/example/pkucat/manage/ManageActivity.java
@@ -13,6 +13,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.pkucat.App;
 import com.example.pkucat.R;
 
 public class ManageActivity extends AppCompatActivity {
@@ -36,79 +37,22 @@ public class ManageActivity extends AppCompatActivity {
         }
 
         adminUserListView = findViewById(R.id.admin_user_list);
-        adminUserList = new AdminUserList(this);
+        App app = (App)getApplication();
+        adminUserList = new AdminUserList(this, app.client);
         adminUserListView.setAdapter(adminUserList);
-
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.manage_menu, menu);
         return true;
-    }
-
-    private int addUser(final String userName, final int level)
-    {
-        final String level_name = (level==1)?("饲养员"):("管理员");
-        int errno = 0;
-        String hintMsg = level_name+"设置成功！";
-        if(userName.equals("")){hintMsg = "用户名不能为空！";errno = -1;}
-        else if (adminUserList.contains(userName)){hintMsg = "该用户已有权限！";errno = -1;}
-        else {
-            adminUserList.putItem(userName, level);
-            adminUserList.notifyDataSetChanged();
-        }
-        Toast.makeText(ManageActivity.this,hintMsg,Toast.LENGTH_SHORT).show();
-        return errno;
-    }
-
-    private void addUserWindow(final int level)
-    {
-        final String level_name = (level==1)?("饲养员"):("管理员");
-        final EditText inputName = new EditText(this);
-        int errno = 0;
-        inputName.setSingleLine(true);
-        inputName.setHorizontallyScrolling(true);
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("设置"+level_name);
-        inputName.setHint("请输入用户名");
-        builder.setView(inputName);
-        builder.setCancelable(false);
-        builder.setPositiveButton("确定", new DialogInterface.OnClickListener(){
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                String userName = inputName.getText().toString();
-                addUser(userName, level);
-            }
-        });
-        builder.setNegativeButton("取消", null);
-        builder.show();
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch (item.getItemId())
-        {
-            case R.id.manage_button_add_level1:
-                addUserWindow(1);break;
-            case R.id.manage_button_add_level2:
-                addUserWindow(2);break;
-            case R.id.manage_button_sort_by_name:
-                adminUserList.sort_by_name();
-                adminUserList.notifyDataSetChanged();
-                break;
-            case R.id.manage_button_sort_by_level:
-                adminUserList.sort_by_level();
-                adminUserList.notifyDataSetChanged();
-                break;
-
+    }*/
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
             case android.R.id.home:
-                // 返回键
                 this.finish(); // back button
                 return true;
-
-            default:break;
         }
         return super.onOptionsItemSelected(item);
     }
